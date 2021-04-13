a092ac09c90ab74bfc72abfecb0ceacc96e79ed4
Tiago Leal <tibeal@gmail.com>
2021-04-13 10:31:19 -0300

    enhancements

--- force-app/main/default/lwc/ccSimpleSimulator/ccSimpleSimulator.js
+++ force-app/main/default/lwc/ccSimpleSimulator/ccSimpleSimulator.js
@@ -2,6 +2,7 @@ import { LightningElement, track, api } from 'lwc';
 import getRecords from '@salesforce/apex/SimpleSimulatorController.getRecords';
 import translateIds from '@salesforce/apex/SimpleSimulatorController.translateIds';
 import simulate from '@salesforce/apex/SimpleSimulatorController.simulate';
+import getConfiguration from '@salesforce/apex/SimpleSimulatorController.getConfiguration';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CcSimpleSimulator extends LightningElement {
@@ -12,16 +13,21 @@ export default class CcSimpleSimulator extends LightningElement {
   @track hasMember = false;
   @track hasRecords = false;
   @track hasSelectedRecords = false;
+  @track hasOutput = false;
   @track objectValue;
   @track output = '';
   @track filters = '';
 
   @track relatedRecords = [];
   @track relatedColumns = [];
+  @track outputColumns = [];
   @track selectedIds;
 
-  @track translate = false;
+  @track rows;
+
   @track showOutput = false;
+
+  config = {};
 
   @api objectName;
 
@@ -36,9 +42,9 @@ export default class CcSimpleSimulator extends LightningElement {
       this.member.id = this.member.Id;
       console.log(`member: " ${JSON.stringify(this.member, null, 2)}`);
       this.getRelatedRecords();
+      this.getFieloConfiguration();
     }
   }
-
 
   getRelatedRecords(){
     getRecords({
@@ -90,64 +96,73 @@ export default class CcSimpleSimulator extends LightningElement {
       records: this.selectedIds
     })
     .then(output => {
-      this.translate = this.template.querySelector('.fielo-translate-field').checked;
+      var idsToTranslate = [];
+      var outputObj = JSON.parse(output);
 
-      console.log(`this.translate: ${this.translate}`);
-      if (!this.translate) {
-        this.output = output;
-        this.showOutput = true;
-        this.jsonToTable(JSON.parse(this.output));
-      } else {
-        var idsToTranslate = [];
-        var outputObj = JSON.parse(output);
-
-        Object.keys(outputObj).forEach(currencyId => {
-          idsToTranslate.push(currencyId);
-          Object.keys(outputObj[currencyId].records).forEach(recordId => {
-            idsToTranslate.push(recordId);
-            Object.keys(outputObj[currencyId].records[recordId].incentives).forEach(incentiveId => {
-              idsToTranslate.push(incentiveId);
-            })
+      Object.keys(outputObj).forEach(currencyId => {
+        idsToTranslate.push(currencyId);
+        Object.keys(outputObj[currencyId].records).forEach(recordId => {
+          idsToTranslate.push(recordId);
+          Object.keys(outputObj[currencyId].records[recordId].incentives).forEach(incentiveId => {
+            idsToTranslate.push(incentiveId);
           })
         })
+      })
 
-        var outputStr = output;
+      var outputStr = output;
 
-        translateIds({idsToTranslate: idsToTranslate})
-        .then(translationMap => {
-          Object.keys(translationMap).forEach(fObjectId => {
-            outputStr = outputStr.replaceAll(fObjectId, translationMap[fObjectId]);
-          });
-          this.output = outputStr;
-          this.showOutput = true;
-          this.jsonToTable(JSON.parse(this.output));
-        })
-        .catch(error => {
-          console.error(error);
-          const errorEvent = new ShowToastEvent({
-            title: 'Translation Error',
-            message: error && error.body && error.body.message || JSON.stringify(error),
-            variant: 'error',
-            mode: 'dismissable'
-          });
-          this.dispatchEvent(errorEvent);
-        })
-      }
+      translateIds({idsToTranslate: idsToTranslate})
+      .then(translationMap => {
+        Object.keys(translationMap).forEach(fObjectId => {
+          outputStr = outputStr.replaceAll(fObjectId, translationMap[fObjectId]);
+        });
+        this.output = outputStr;
+        this.showOutput = true;
+        this.jsonToTable(JSON.parse(this.output));
+      })
+      .catch(error => {
+        console.error(error);
+        const errorEvent = new ShowToastEvent({
+          title: 'Translation Error',
+          message: error && error.body && error.body.message || JSON.stringify(error),
+          variant: 'error',
+          mode: 'dismissable'
+        });
+        this.dispatchEvent(errorEvent);
+      })
     })
     .catch(error => {
-      console.error(error);
-      const errorEvent = new ShowToastEvent({
+      this.handleError(console.error());
+    })
+  }
+
+  getFieloConfiguration() {
+    getConfiguration({
+      memberId: this.member.Id
+    })
+    .then(result => {
+      this.config = result;
+      console.info(JSON.stringify(this.config, null, 2));
+      this.outputColumns = [...this.config.columns];
+    })
+    .catch(error => {
+      this.handleError(error);
+    });
+  }
+
+  handleError(error) {
+    console.error(error);
+    const errorEvent = new ShowToastEvent({
         title: 'Simulation Error',
         message: error && error.body && error.body.message || JSON.stringify(error),
         variant: 'error',
         mode: 'dismissable'
       });
       this.dispatchEvent(errorEvent);
-    })
   }
 
   jsonToTable(result) {
-    var rows = [];
+    this.rows = [];
     Object.keys(result).forEach(curr => {
       let records = result[curr].records;
       Object.keys(records).forEach(record => {
@@ -165,9 +180,12 @@ export default class CcSimpleSimulator extends LightningElement {
             console.log(
               JSON.stringify(row, null, 2)
             );
+            this.rows.push(row);
           });
         });
       });
     });
+
+    this.hasOutput = Boolean(this.rows && this.rows.length);
   }
 }
